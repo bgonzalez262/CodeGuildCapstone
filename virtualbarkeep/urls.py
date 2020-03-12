12bb75284167fdbf1721cef331c97a6d5db4839a
bgonzalez262 <bgonzo262@gmail.com>
2020-03-11 22:41:06 -0700

    users, change to vbk, reglog page

--- virtualbarkeep/urls.py
+++ virtualbarkeep/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'virtualbarkeep'
+app_name = 'vbk'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.searchalc, name='searchalc')
+    path('search/', views.searchalc, name='searchalc'),
+    path('register/', views.register, name='register' ),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+
 ]
--- virtualbarkeep/views.py
+++ virtualbarkeep/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -7,3 +11,41 @@ def index(request):
 def searchalc(request):
     return render(request,'virtualbarkeep/search.html')
 
+def register(request):
+    # if request.method == 'GET':
+    return render(request, 'virtualbarkeep/reglog.html')
+    # username = request.POST['username']
+    # email = request.POST['email']
+    # password = request.POST['password']
+    # user = User.objects.create_user(username, email, password)
+
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'virtualbarkeep/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    next = request.GET.get('next','')
+    print(request.POST)
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request,user)
+        if next != '':
+            return HttpResponseRedirect(next)
+        return HttpResponseRedirect(reverse('vbk:index'))
+    if next == '':
+        return HttpResponseRedirect(reverse('vbk:login') + '?message=failure')
+    return HttpResponseRedirect(reverse('vbk:profile') + '?message=failure&next='+next)
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('vbk:index') + '?message=loggedout')
+
+@login_required
+def profile(request):
+    return render(request, 'vbk/reglog.html')
+
+'''TODO
+-finish adding the users and test register page
+-set up "add to" feature, view and profile page
+-model creation
+'''
